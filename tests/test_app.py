aad1961be704d90e06307f5238eba0426e0e7072
Gustavo Brito <britogustaff@gmail.com>
2026-05-13 21:01:48 -0300

    feat: adiciona exercicio 02 do curso

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,19 @@ from fastapi.testclient import TestClient
 from fastapi_zero.app import app
 
 
-def test_root_deve_retornar_ola_mundo():
+def test_exercicio_ola_mundo_em_html():
     client = TestClient(app)
 
-    response = client.get('/')
+    response = client.get('/exercicio-html')
 
-    assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo </h1>' in response.text
+
+
+# def test_root_deve_retornar_ola_mundo():
+#    client = TestClient(app)
+#
+#    response = client.get('/')
+#
+#    assert response.json() == {'message': 'Olá mundo!'}
+#    assert response.status_code == HTTPStatus.OK
